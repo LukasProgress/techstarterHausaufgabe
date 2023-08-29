74ef853b8148bbcc43ad50648f5989714493975b
FrodinLP <ingo.neubert92@gmx.de>
2023-08-29 13:23:49 +0200

    Hausaufgaben reloaded

--- HA_29_08/main.js
+++ HA_29_08/main.js
@@ -4,11 +4,21 @@
 function makeUsers() {
     // TODO 2. Lösche den standard user und Füge deine eigenen User ein:
     const admin = {
-        name: "admin",
+        name: "ali",
         pw: "123"
-    }
-    
-    var allUsers = [admin];
+    };
+
+    const user2 = {
+        name: "dirk",
+        pw: "456"
+    };
+
+    const user3 = {
+        name: "henry",
+        pw: "789"
+    };
+
+    var allUsers = [ali, dirk, henry];
 
 
     return allUsers;
@@ -32,5 +42,25 @@ function login(){
 function validLogin(username, password){
     allUsers = makeUsers();
     // TODO: 3. Programmiere die Funktion validLogin
-    return true
+    const users = [
+        { username: "ali", password: "123" },
+        { username: "dirk", password: "456" },
+        { username: "henry", password: "789" }
+    ];
+
+    for (const user of users) {
+        if (user.username === username && user.password === password) {
+            return true; // Valid login
+        }
+    }
+
+    return false; // Invalid login
+}
+const inputUsername = "user1";
+const inputPassword = "wrongpass";
+
+if (validLogin(inputUsername, inputPassword)) {
+    console.log("Login erfolgreich");
+} else {
+    console.log("Login fehlgeschlagen, prüfe deine Einloggdaten.");
 }
