c01dcff169790852efcf652f4923048eea1480b1
AndreasKort <andreas.kort@docc.techstarter.de>
2023-08-29 19:06:59 +0200

    gefundenen Fehler behoben

--- HA_29_08/main.js
+++ HA_29_08/main.js
@@ -1,8 +1,7 @@
 //LOGIN SCRIPT
 
-
+// Erstelle 3 Benutzer und füge sie in das Array allUsers ein
 function makeUsers() {
-    // Erstelle 3 Benutzer und füge sie in das Array allUsers ein
     const user1 = {
         name: "Nummer1",
         pw: "12345"
@@ -20,39 +19,30 @@ function makeUsers() {
 
     var allUsers = [user1, user2, user3];
 
-    // Entferne den Admin-Benutzer mit Passwort "123"
-    allUsers = allUsers.filter(user => !(user.name === "admin" && user.pw === "123"));
-
     return allUsers;
-}// TODO 2. Lösche den standard user und Füge deine eigenen User ein:
-    const admin = {
-        name: "admin",
-        pw: "123"
-    }
-    
-    var allUsers = [admin];
+}
 
-
-    return allUrs;
-
-
-
-// Die Funktion login wird vom HTML Dokument aus aufgerufen, sobald der login Button gedrückt wird
+// Die Funktion login wird vom HTML Dokument aus aufgerufen, sobald der Login-Button gedrückt wird
 function login(){
     username = document.getElementsByName("username")[0].value;
     password = document.getElementsByName("password")[0].value;
 
-    //Nachdem username und Passwort ausgelesen werden, wird die validität überprüft. Passt diese, verändern wir den HTML code von "body"
+    // Nachdem Benutzername und Passwort ausgelesen werden, wird die Validität überprüft. Passt diese, verändern wir den HTML-Code von "body"
     if (validLogin(username, password)){
         document.getElementById("body").innerHTML = `<h1>Welcome, ${username}!</h1>`;
     }
     return undefined;
 }
 
-// Die Funktion validLogin wird ein gegebenes username-password paar nach validität überprüfen
+// Die Funktion validLogin wird ein gegebenes Benutzername-Passwort-Paar nach Validität überprüfen
 // Als Ergebnis muss validLogin einen Boolean zurückgeben!
 function validLogin(username, password){
-    allUsers = makeUsers();
-    // TODO: 3. Programmiere die Funktion validLogin
-    return true
+    const allUsers = makeUsers();
+    
+    // Hier wird der Admin-Benutzer mit Passwort "123" entfernt
+    allUsers = allUsers.filter(user => !(user.name === "admin" && user.pw === "123"));
+
+    // Jetzt können wir die Validität überprüfen
+    const user = allUsers.find(user => user.name === username && user.pw === password);
+    return !!user; // Gibt true zurück, wenn ein übereinstimmender Benutzer gefunden wurde, andernfalls false
 }
