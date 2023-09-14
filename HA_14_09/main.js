6d3aca461bb3ce3508657733582df6d9a55cd5ef
Timour <sagichdirnicht259@outlook.de>
2023-09-14 23:16:17 +0200

    hakomplett

--- HA_14_09/main.js
+++ HA_14_09/main.js
@@ -1,46 +1,38 @@
-//------------------------------------------------------------------------------ 
-// CLASSES
-
-//TODO: 1. Erstelle die beiden Klassen "Department" und "Employee"
+// CLASSES ////N IFDSIFNIDSFNSOIDFN Irgendwie hab ich die Aufgabe vollkommen zerschossen, mir fehlte leider die Zeit um von vorn zu beginnen
 class Company {
-    constructor(name){
+    constructor(name) {
         this.name = name;
     }
 }
 
 class Department {
-    constructor(name, company, boss) {
-        this.name = name;
+    constructor(company, name, boss) {
         this.company = company;
+        this.name = name;
         this.boss = boss;
     }
 }
 
 class Employee {
-    constructor(name, department, salary) {
-        this.name = name;
+    constructor(department, name, salary) {
         this.department = department;
+        this.name = name;
         this.salary = salary;
     }
 }
 
-
-
-
 //==============================================================================
 // OBJECT INSTANCES
+
 const bigMoneyCorp = new Company("Big Money Corporations");
 
-
 let finance = new Department(bigMoneyCorp, "Finance", "Gerhard Geldsack");
-let hr = new Department(bigMoneyCorp, "Human Ressources", "Albrecht Aasgeier");
+let hr = new Department(bigMoneyCorp, "Human Resources", "Albrecht Aasgeier");
 let marketing = new Department(bigMoneyCorp, "Marketing", "Magnus McMamakind");
 
-//TODO: 2. Füge ein weiteres Department hinzu (eignes)
-let tech = new Department(bigmoneyCorp, "Tech", "Timour Miagol");
+let tech = new Department(bigMoneyCorp, "Tech", "Timour Miagol");
 
-//Wir speichern alle angestellten in einem einzigen Array:
-let allEmployees = []
+let allEmployees = [];
 allEmployees.push(new Employee(finance, "Gerhard Geldsack", 2000000));
 allEmployees.push(new Employee(hr, "Albrecht Aasgeier", 3500000));
 allEmployees.push(new Employee(marketing, "Magnus McMamakind", 2350000));
@@ -48,99 +40,74 @@ allEmployees.push(new Employee(finance, "Lukas Probst", 45000));
 allEmployees.push(new Employee(finance, "Marcus Wunderle", 46000));
 allEmployees.push(new Employee(finance, "Andreas Armschlucker", 29500));
 allEmployees.push(new Employee(finance, "Roman Relativarm", 30400));
-// FÜR Aufgabe 2  Chef der mehr verdient
-allEmployees.push(new Employee(tech, "Timour Miagol", 2000000))
-// ANGEllstellte des neuen departments
-allEmployees.push(new Employee(tech, "NPC1", 80000))
-allEmployees.push(new Employee(tech, "NPC2", 80000))
-allEmployees.push(new Employee(tech, "NPC3", 80000))
-// Neue engestellten für hr
-allEmployees.push(new Employee(hr, "hrNPC1", 70000))
-allEmployees.push(new Employee(hr, "hrNPC2", 60000))
-allEmployees.push(new Employee(hr, "hrNPC3", 65000))
-//neue angestellten für marketing
-allEmployees.push(new Employee(marketing, "marketingNPC1", 65000))
-allEmployees.push(new Employee(marketing, "marketingNPC2", 65000))
-allEmployees.push(new Employee(marketing, "marketingNPC3", 65000))
 
+allEmployees.push(new Employee(tech, "Timour Miagol", 2000000));
+allEmployees.push(new Employee(tech, "NPC1", 80000));
+allEmployees.push(new Employee(tech, "NPC2", 80000));
+allEmployees.push(new Employee(tech, "NPC3", 80000));
 
+allEmployees.push(new Employee(hr, "hrNPC1", 70000));
+allEmployees.push(new Employee(hr, "hrNPC2", 60000));
+allEmployees.push(new Employee(hr, "hrNPC3", 65000));
 
+allEmployees.push(new Employee(marketing, "marketingNPC1", 65000));
+allEmployees.push(new Employee(marketing, "marketingNPC2", 65000));
+allEmployees.push(new Employee(marketing, "marketingNPC3", 65000));
 
-
-// TODO: 2. Füge mindestens je 3 neue Angestellte bei hr und marketing, sowie eurem department hinzu.
 //==============================================================================
 // FUNCTIONS
 
-
-
-// + Mitarbeiter des Departments
-
-
-
-// 3 neue angestellte für HR
-
-
-
-
-
-// FUNCTIONS
-function getBoss(department) {
-    return department.boss;
-  }
-  
-  
-  
-  function raiseIncome(employee, amount) {
-    employee.salary = amount + employee.solary;
-  }
-  
-
-  
-  function doubleBossIncome(department) {
-    const boss = getBoss(department);
-    raiseIncome(boss, boss.salary);
-  }
-  
-  
-  function averageIncome(employees) {
-    let totalSalary = 0;
-    for (let employee of employees) {
-      totalSalary += employee.salary;
+function getBoss(employeeName) {
+    const employee = allEmployees.find(employee => employee.name === employeeName);
+    if (employee) {
+      const department = employee.department;
+      if (department && department.boss === employee) {
+        return department.boss;
+      }
     }
-    return totalSalary / employees.length;
+    return null;
   }
 
+function raiseIncome(department, amount) {
+    const employeesInDepartment = allEmployees.filter(employee => employee.department === department);
+    const employeesWithoutBoss = employeesInDepartment.filter(employee => employee.name !== department.boss);
+    const raise = amount / employeesWithoutBoss.length;
+    employeesWithoutBoss.forEach(employee => employee.salary += raise);
+}
 
-//TODO: 4. Funktion getBoss
-
-//TODO: 5. Funktion raiseIncome
-
-//TODO: 6. doubleBossIncome
-
-//TODO: 7. averageIncome
-
-
-
-
+function averageIncome(department) {
+    const employeesInDepartment = allEmployees.filter(employee => employee.department === department);
+    const salaries = employeesInDepartment.map(employee => employee.salary);
+    const totalSalary = salaries.reduce((a, b) => a + b, 0);
+    return totalSalary / salaries.length;
+}
 
 //==============================================================================
-//main function (Diese rufen wir auf, wenn die Datei ausgeführt wird.)
-function main(){
-    //TODO: 3. Gib die Namen aller Angestellter auf der Konsole aus:
+// MAIN FUNCTION
 
-    //TODO: 3. Gib die Namen aller Angestellter in hr auf der Konsole aus:
+function main() {
+    let names = allEmployees.map(employee => employee.name);
+    console.log(names);
 
-    //TODO: 3. Gib die Namen aller Angestellten mit einem Gehalt von mehr als 35.000 im Jahr aus
-    
-    //TODO: 4. Nutze die Funktion getBoss() um den Chef von "Andreas Armschlucker" zu finden und auszugeben.
+    const hrEmployees = allEmployees.filter(employee => employee.department.name === "Human Resources");
+    const hrNames = hrEmployees.map(employee => employee.name);
+    console.log(hrNames);
 
-    //TODO: 5. Nutze die Funktion raiseIncome() um allen Angestellten, die in "Human Ressources" arbeiten (Außer dem chef) 
-    //         eine Gehaltserhöhung von 4000 im Jahr zu geben
+    const highSalaryEmployees = allEmployees.filter(employee => employee.salary > 35000);
+    const highSalaryNames = highSalaryEmployees.map(employee => employee.name);
+    console.log(highSalaryNames);
 
-    //TODO: 6. Nutze die Funktion doubleBossIncome() um dem Boss von HR das doppelte gehalt zu geben
+    const armschluckersBoss = getBoss(armschlucker.name);
+    if (armschluckersBoss) {
+      console.log(armschluckersBoss.name);
+    } else {
+      console.log("Chef nicht gefunden.");
+    }
 
-    //TODO: 7. Nutzt averageIncome() um euch das Durschnittsgehalt eures eigenen Departments ausgeben zu lassen.
-    
+    raiseIncome(hr, 4000);
+
+    const techAverageSalary = averageIncome(tech);
+    console.log(`Durchschnittsgehalt im Tech-Department: ${techAverageSalary}`);
 }
 
 main();
--- HA_14_09/task2/script.js
+++ HA_14_09/task2/script.js
@@ -1,15 +1,45 @@
-function add(a, b){
-    return a + b
-}
 
-function sub(a, b){
-    return a - b
-}
+window.addEventListener("load", function() { // ging besser, das war super easy
 
-function mul(a, b){
-    return a * b
-}
+    // nötige Variablen deklarieren
+let value1 = document.getElementById("number1");
+let value2 = document.getElementById("number2");
+const addButton = document.querySelector('input[type="button"][value="+"]');
+const subtractButton = document.querySelector('input[type="button"][value="-"]');
+const multiplyButton = document.querySelector('input[type="button"][value="*"]');
+const divideButton = document.querySelector('input[type="button"][value="/"]');
 
-function div(a, b){
-    return a / b
-}
+
+
+addButton.addEventListener("click", () => {
+
+    const result = parseInt(value1.value) + parseInt(value2.value);
+const resultSpan = document.getElementById("result");
+resultSpan.innerText = result;
+  })
+  
+
+subtractButton.addEventListener("click", () => {
+
+const result = parseInt(value1.value) - parseInt(value2.value);
+const resultSpan = document.getElementById("result");
+resultSpan.innerText = result;
+  })
+  
+  multiplyButton.addEventListener("click", () => {
+
+    const result = parseInt(value1.value) * parseInt(value2.value);
+    const resultSpan = document.getElementById("result");
+    resultSpan.innerText = result;
+      })
+      
+
+      divideButton.addEventListener("click", () => {
+
+        const result = parseInt(value1.value) / parseInt(value2.value);
+        const resultSpan = document.getElementById("result");
+        resultSpan.innerText = result;
+          })
+
+
+});
