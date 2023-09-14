76b32830f9e113920e5df11a7d98b5657a21f4dd
Lev2607 <levent.o@web.de>
2023-09-14 13:20:44 +0200

    Aufgabe  2

--- HA_14_09/main.js
+++ HA_14_09/main.js
@@ -24,12 +24,6 @@ class Employee {
     }
 }
 
-
-
-
-
-
-
 //==============================================================================
 // OBJECT INSTANCES
 const bigMoneyCorp = new Company("Big Money Corporations");
@@ -37,7 +31,10 @@ const bigMoneyCorp = new Company("Big Money Corporations");
 let finance = new Department(bigMoneyCorp, "Finance", "Gerhard Geldsack");
 let hr = new Department(bigMoneyCorp, "Human Ressources", "Albrecht Aasgeier");
 let marketing = new Department(bigMoneyCorp, "Marketing", "Magnus McMamakind");
+
 //TODO: 2. Füge ein weiteres Department hinzu
+
+let it = new Department(bigMoneyCorp, "IT", "boss");
 
 //Wir speichern alle angestellten in einem einzigen Array:
 let allEmployees = []
@@ -48,8 +45,23 @@ allEmployees.push(new Employee(finance, "Lukas Probst", 45000));
 allEmployees.push(new Employee(finance, "Marcus Wunderle", 46000));
 allEmployees.push(new Employee(finance, "Andreas Armschlucker", 29500));
 allEmployees.push(new Employee(finance, "Roman Relativarm", 30400));
+allEmployees.push(new Employee(it, "boss", 5000000));
+
 // TODO: 2. Füge mindestens je 3 neue Angestellte bei hr und marketing, sowie eurem department hinzu.
-//==============================================================================
+
+allEmployees.push(new Employee(hr, "Max Mustermann", 40000));
+allEmployees.push(new Employee(hr, "Martina Musterfrau", 45000));
+allEmployees.push(new Employee(hr, "Klaus Kleber", 50000));
+allEmployees.push(new Employee(marketing, "Bibi Bloxberg", 40000));
+allEmployees.push(new Employee(marketing, "Benjamin Blümchen", 45000));
+allEmployees.push(new Employee(marketing, "Harry Potter", 50000));
+allEmployees.push(new Employee(it, "Bernd Stromberg", 40000));
+allEmployees.push(new Employee(it, "Walter White", 45000));
+allEmployees.push(new Employee(it, "Clark Kent", 50000));
+
+
+
+
 // FUNCTIONS
 
 //TODO: 4. Funktion getBoss
