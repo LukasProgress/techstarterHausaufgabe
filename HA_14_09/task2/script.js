f084926be18b72c5a82a8cf4ca75ca5a1e4a4b1c
dyprodg <dennisdiepolder@outlook.com>
2023-09-14 19:15:12 +0200

    fuer heute fertig

--- HA_14_09/task2/script.js
+++ HA_14_09/task2/script.js
@@ -1,5 +1,6 @@
 function add(a, b){
-    return a + b
+    return a + b;
+
 }
 
 function sub(a, b){
@@ -12,4 +13,32 @@ function mul(a, b){
 
 function div(a, b){
     return a / b
+}
+
+document.getElementById("addButton").onclick = function() {
+    const a = parseFloat(document.getElementById('number1').value);
+    const b = parseFloat(document.getElementById('number2').value);
+    const result = add(a,b);
+    document.getElementById('resultOutput').innerText = result;
+}
+
+document.getElementById("subtractButton").onclick = function() {
+    const a = parseFloat(document.getElementById('number1').value);
+    const b = parseFloat(document.getElementById('number2').value);
+    const result = sub(a,b);
+    document.getElementById('resultOutput').innerText = result;
+}
+
+document.getElementById("multiplyButton").onclick = function() {
+    const a = parseFloat(document.getElementById('number1').value);
+    const b = parseFloat(document.getElementById('number2').value);
+    const result = mul(a,b);
+    document.getElementById('resultOutput').innerText = result;
+}
+
+document.getElementById("divideButton").onclick = function() {
+    const a = parseFloat(document.getElementById('number1').value);
+    const b = parseFloat(document.getElementById('number2').value);
+    const result = div(a,b);
+    document.getElementById('resultOutput').innerText = result;
 }
