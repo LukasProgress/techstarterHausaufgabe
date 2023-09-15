9f434cecf03d393dccc87dd3d1acab5af0491728
PabloJoelMR <pabloreufer@gmail.com>
2023-09-15 09:20:49 +0200

    main partial

--- HA_14_09/main.js
+++ HA_14_09/main.js
@@ -37,10 +37,13 @@ let hr = new Department(bigMoneyCorp, "Human Ressources", "Albrecht Aasgeier");
 let marketing = new Department(bigMoneyCorp, "Marketing", "Magnus McMamakind");
 let it = new Department(bigMoneyCorp, "IT", "Lukas Luxusmensch" )
 
+const allDepartments = []
+allDepartments.push(finance, hr, marketing, it)
+
 //TODO: 2. Füge ein weiteres Department hinzu
 
 //Wir speichern alle angestellten in einem einzigen Array:
-let allEmployees = []
+const allEmployees = []
 allEmployees.push(new Employee(finance, "Gerhard Geldsack", 2000000));
 allEmployees.push(new Employee(hr, "Albrecht Aasgeier", 3500000));
 allEmployees.push(new Employee(marketing, "Magnus McMamakind", 2350000));
@@ -62,7 +65,27 @@ allEmployees.push(new Employee(marketing, "Sarah Sagtnichviel", 45000));
 //==============================================================================
 // FUNCTIONS
 
+// Namen aller Angestellten
+const names = () => {allEmployees.map(x => x.name)};
+
+// Namen aller Angestellten im Bereich HR
+const hrnames = () => {allEmployees.filter(x => x.department === hr).map(x => x.name)};
+
+// Namen aller Angestellten mit Gehalt > 35000
+const highsalarynames = () => {allEmployees.filter(x => x.salary > 35000).map(x => x.name)};
+
+
 //TODO: 4. Funktion getBoss
+const bosses = allDepartments.map(x => x.boss)
+
+
+function getBoss(name) {
+    return allDepartments.map(x => x.name.boss === name)    
+}
+
+
+console.log(getBoss("Andreas Armschlucker"))
+
 
 //TODO: 5. Funktion raiseIncome
 
@@ -78,11 +101,11 @@ allEmployees.push(new Employee(marketing, "Sarah Sagtnichviel", 45000));
 //main function (Diese rufen wir auf, wenn die Datei ausgeführt wird.)
 function main(){
     //TODO: 3. Gib die Namen aller Angestellter auf der Konsole aus:
-
+    console.log(names)
     //TODO: 3. Gib die Namen aller Angestellter in hr auf der Konsole aus:
-
+    console.log(hrnames)
     //TODO: 3. Gib die Namen aller Angestellten mit einem Gehalt von mehr als 35.000 im Jahr aus
-    
+    console.log(highsalarynames)
     //TODO: 4. Nutze die Funktion getBoss() um den Chef von "Andreas Armschlucker" zu finden und auszugeben.
 
     //TODO: 5. Nutze die Funktion raiseIncome() um allen Angestellten, die in "Human Ressources" arbeiten (Außer dem chef) 
