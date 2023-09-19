af33b5cace03f28e9aae8da7ade34ba1e05c27b2
RaupeCHR <raupechr@gmail.com>
2023-09-19 14:24:42 +0200

    tüftleraufgabe gemacht ;)

--- HA_19_09/index.js
+++ HA_19_09/index.js
@@ -5,14 +5,19 @@ const port = 3000;
 
 
 app.get('/', (req, res) => {
-  res.send('Herzlich Willkommen zu den Hausaufgaben von Christoph Raupach!');
+  res.send(`
+  <h1>Herzlich Willkommen zu den Hausaufgaben von Christoph Raupach!</h1>
+  <a href="/">Home</a>
+  <a href="/oldhtml">To Do List</a>
+  <a href="/cat/:says">Catpictures</a>
+  `);
 });
 
 app.get('/oldhtml', (req, res) => {
     res.sendFile('todo.html', { root: './'});
 });
 
-app.get('/cat/says:', (req, res) => {
+app.get('/cat/:says', (req, res) => {
   const text = req.params.says;
   res.redirect(`https://cataas.com/cat/says/${text}`);
 });
