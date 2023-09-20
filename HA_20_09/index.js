2a736cd7b03d2f8fed465819be65550ea88771dc
Dominik <dominikwieland@mein.gmx>
2023-09-20 14:13:48 +0200

    HA_20_09_A2

--- HA_20_09/index.js
+++ HA_20_09/index.js
@@ -1,43 +1,85 @@
-import express from "express"
-import bodyParser from "body-parser"
+import express from "express";
+import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
 
-const port = 3000
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
+const port = 3000;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const form = `
+const loginForm = `
 <form method="post" action="/login">
     <label for="name">Name:</label>
     <input name="name" type="text">
     <label for="pw">Passwort:</label>
     <input name="pw" type="password">
     <button type="submit">Login</button>
+    <a href="/register">Registrieren</a> <!-- Hier wurde der Registrieren-Link hinzugefügt -->
 </form>
-`
+`;
 
-const user = [
-    {name: "Lukas", passwort: "123"},
-    {name: "Bartek", passwort: "321"},
-    {name: "Abi", passwort: "Banane"}
-]
+const user = [];
+
+// route 
+const registerForm = `
+<form method="post" action="/register">
+    <label for="name">Name:</label>
+    <input name="name" type="text">
+    <label for="pw">Passwort:</label>
+    <input name="pw" type="password">
+    <label for="confirmPw">Passwort wiederholen:</label>
+    <input name="confirmPw" type="password">
+    <button type="submit">Registrieren</button>
+</form>
+`;
 
 app.get("/", (req, res) => {
-    res.send(form)
-})
+    res.send(loginForm);
+});
+
+app.get("/register", (req, res) => {
+    res.send(registerForm);
+});
 
 app.post("/login", (req, res) => {
-    const {name, pw} = req.body
+    const { name, pw } = req.body;
 
-    for (let i = 0; i < user.length; i++){
-        if (name === user[i].name && pw === user[i].passwort){
-            res.send(`Login erfolgreich`)
-            return
-        } 
+    for (let i = 0; i < user.length; i++) {
+        if (name === user[i].name && bcrypt.compareSync(pw, user[i].hashedPassword)) {
+            res.send(`Login erfolgreich`);
+            return;
+        }
     }
 
-    res.send(`Login fehlgeschlagen`)
-}) 
+    res.send(`Login fehlgeschlagen`);
+});
+
+app.post("/register", (req, res) => {
+    const { name, pw, confirmPw } = req.body;
+
+    // existence of User
+    for (let i = 0; i < user.length; i++) {
+        if (name === user[i].name) {
+            res.send(`Benutzername bereits vergeben`);
+            return;
+        }
+    }
+
+    // password compare
+    if (pw !== confirmPw) {
+        res.send(`Passwörter stimmen nicht überein`);
+        return;
+    }
+
+    // safe hash
+    const saltRounds = 10; // Anzahl der Hashing-Runden
+    const hashedPassword = bcrypt.hashSync(pw, saltRounds);
+
+    // New User
+    user.push({ name, hashedPassword });
+
+    res.send(`Registrierung abgeschlossen`);
+});
 
 app.listen(port, () => {
-    console.log("server listens on port", port)
-})
+    console.log("Server listens on port", port);
+});
