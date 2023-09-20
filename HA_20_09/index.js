138fcae3ef76dd8a34808109ee4c62d888308b97
Enrico Carteciano <carteciano.enrico@gmail.com>
2023-09-20 15:36:11 +0200

    HA_20_09

--- HA_20_09/index.js
+++ HA_20_09/index.js
@@ -1,10 +1,17 @@
+// Importieren der notwendigen Module
 import express from "express"
 import bodyParser from "body-parser"
+import fs from "fs"
+import bcrypt from "bcrypt"
 
-const port = 3000
+// Initialisieren der Express-App und Konfigurieren des Body-Parsers
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// Port-Definition für den Server
+const port = 3000
+
+// HTML-Formulare als Strings
 const form = `
 <form method="post" action="/login">
     <label for="name">Name:</label>
@@ -13,31 +20,107 @@ const form = `
     <input name="pw" type="password">
     <button type="submit">Login</button>
 </form>
+<a href="/register"><button type="button">Registrieren</button></a>
 `
 
-const user = [
-    {name: "Lukas", passwort: "123"},
-    {name: "Bartek", passwort: "321"},
-    {name: "Abi", passwort: "Banane"}
-]
+const formRegistration = `
+<form method="post" action="/register">
+    <label for="name">Name:</label>
+    <input name="name" type="text">
+    <label for="pw">Passwort:</label>
+    <input name="pw" type="password">
+    <label for="pww">Passwort wiederholen:</label>
+    <input name="pww" type="password">
+    <button type="submit">Registrieren</button>
+</form>
+`
 
-app.get("/", (req, res) => {
-    res.send(form)
+// Variable für Benutzerdaten
+let users
+
+// Anzahl der Runden für bcrypt Salting
+const saltRounds = 10
+
+// Laden der Benutzerdaten beim Start des Servers
+fs.readFile("users.json", "utf8", (err, data) => {
+  if (err) {
+    console.error("Fehler beim Lesen der Datei:", err)
+    users = [] // Leeres Array als Fallback
+  } else {
+    if (data) {
+      try {
+        users = JSON.parse(data)
+      } catch (err) {
+        console.error("Ungültiges JSON-Format:", err)
+        users = [] // Leeres Array als Fallback
+      }
+    } else {
+      users = [] // Leeres Array als Fallback
+    }
+  }
 })
 
-app.post("/login", (req, res) => {
-    const {name, pw} = req.body
+// Route für die Homepage und Anzeigen der Login-Form
+app.get("/", (req, res) => {
+  res.send(form)
+})
 
-    for (let i = 0; i < user.length; i++){
-        if (name === user[i].name && pw === user[i].passwort){
-            res.send(`Login erfolgreich`)
-            return
-        } 
+// Route für die Registrationform
+app.get("/register", (req, res) => {
+    res.send(formRegistration)
+  })
+
+// Login-Route
+app.post("/login", async (req, res) => {
+  const { name, pw } = req.body
+
+  for (const existingUser of users) {
+    if (existingUser.name === name) {
+      const match = await bcrypt.compare(pw, existingUser.passwort)
+      if (match) {
+        res.send("Login erfolgreich")
+        return
+      }
     }
+  }
 
-    res.send(`Login fehlgeschlagen`)
-}) 
+  res.send("Login fehlgeschlagen")
+})
 
+// Registrierungs-Route
+app.post("/register", async (req, res) => {
+  const { name, pw, pww } = req.body
+
+  if (pw !== pww) {
+    res.send("Die Passwörter stimmen nicht überein")
+    return
+  }
+
+  for (const existingUser of users) {
+    if (existingUser.name === name) {
+      res.send("Dieser Benutzername ist bereits vergeben")
+      return
+    }
+  }
+
+  // Hashen des Passworts
+  const hashedPassword = await bcrypt.hash(pw, saltRounds)
+
+  const newUser = { name, passwort: hashedPassword }
+  users.push(newUser)
+
+  // Speichern der aktualisierten Benutzerliste
+  fs.writeFile("users.json", JSON.stringify(users), err => {
+    if (err) {
+      console.error("Fehler beim Speichern der Datei:", err)
+      res.send("Registrierung fehlgeschlagen")
+    } else {
+      res.send("Registrierung erfolgreich")
+    }
+  })
+})
+
+// Starten des Servers
 app.listen(port, () => {
-    console.log("server listens on port", port)
+  console.log("Server listens on port", port)
 })
