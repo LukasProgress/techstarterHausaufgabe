428442cc22b487ab0d1d98da99d8ba67a5ca705f
Evelyn Johannsen <evelyn-johannsen@outlook.de>
2023-09-20 21:55:38 +0200

    ergänzte Hausaufgabe

--- HA_20_09/index.js
+++ HA_20_09/index.js
@@ -1,9 +1,9 @@
-import express from "express"
-import bodyParser from "body-parser"
+import express from "express";
+import bodyParser from "body-parser";
 
-const port = 3000
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
+const port = 3000;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const form = `
 <form method="post" action="/login">
@@ -13,31 +13,42 @@ const form = `
     <input name="pw" type="password">
     <button type="submit">Login</button>
 </form>
-`
+`;
+
+// Neuer Button für die Registrierung auf der Startseite
+const homePage = `
+${form}
+<br>
+<form method="get" action="/register">
+    <button type="submit">Registrieren</button>
+</form>
+`;
 
 const user = [
-    {name: "Lukas", passwort: "123"},
-    {name: "Bartek", passwort: "321"},
-    {name: "Abi", passwort: "Banane"}
-]
+    { name: "Lukas", passwort: "123" },
+    { name: "Bartek", passwort: "321" },
+    { name: "Abi", passwort: "Banane" }
+];
 
 app.get("/", (req, res) => {
-    res.send(form)
-})
+    res.send(homePage);
+});
+
+// Restlicher Code bleibt unverändert
 
 app.post("/login", (req, res) => {
-    const {name, pw} = req.body
+    const { name, pw } = req.body;
 
-    for (let i = 0; i < user.length; i++){
-        if (name === user[i].name && pw === user[i].passwort){
-            res.send(`Login erfolgreich`)
-            return
-        } 
+    for (let i = 0; i < user.length; i++) {
+        if (name === user[i].name && pw === user[i].passwort) {
+            res.send(`Login erfolgreich`);
+            return;
+        }
     }
 
-    res.send(`Login fehlgeschlagen`)
-}) 
+    res.send(`Login fehlgeschlagen`);
+});
 
 app.listen(port, () => {
-    console.log("server listens on port", port)
-})
+    console.log("server listens on port", port);
+});
