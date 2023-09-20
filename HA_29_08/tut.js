d9e7987d97d7a571de743fbabed77e2035c1c889
FrodinLP <ingo.neubert92@gmx.de>
2023-09-20 15:49:08 +0200

    erster Teil

--- HA_29_08/tut.js
+++ HA_29_08/tut.js
@@ -9,6 +9,38 @@ class Animal {
     }
 }
 
-let sheep = new Animal("Schaf", 35)
+class Predator extends Animal {
+    constructor(name, weight, dangerFactor = 1){
+        super(name, weight)
+        this.dangerFactor = dangerFactor
+    }
+    fightPower(){
+        return weight * dangerFactor
+    }
+    feed(target){
+        if (target instanceof Animal){
+            this.weight += target.weight
+            target.weight = 0
+            target.alive = false
+            
+        }
+        else {
+            console.log("Das Ziel ist kein Tier")
+        }
+        
+    }
+}
+
+class Prey extends Animal {
+
+}
+
+
+let sheep = new Prey("Schaf", 35)
+let lion = new Predator("Löwe", 250)
 sheep.feed(3)
-console.log(sheep.weight)
+//console.log(sheep.weight)
+//console.log(lion.weight)
+lion.feed(sheep)
+console.log(lion)
+console.log(sheep)
