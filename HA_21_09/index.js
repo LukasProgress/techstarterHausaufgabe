438972c79ba8f75aaa88f1b19c9c3a1116712827
RaupeCHR <raupechr@gmail.com>
2023-09-21 12:50:45 +0200

    Erstellen von HA_21_09

--- HA_21_09/index.js
+++ HA_21_09/index.js
@@ -1,1 +1,38 @@
-console.log("fun")
+import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get(`/`, (req, res) => {
+    res.send("Hi Lukas!");
+
+});
+
+const books = [
+    {
+        id: 1,
+        title: 'Moby Dick',
+        author: 'Herman Melville',
+        source: 'techstarterHausaufgabe\HA_21_09\Moby-Dick.txt'
+    },
+    {
+        id: 2,
+        title: 'Frankenstein',
+        author: 'Mary Wollstonecraft Shelley',
+        source: 'techstarterHausaufgabe\HA_21_09\Frankenstein.txt'
+    },
+    {
+        id: 2,
+        title: 'The Strange Case of Dr. Jekyll and Mr. Hyde',
+        author: 'Robert Louis Stevenson',
+        source: 'techstarterHausaufgabe\HA_21_09\The Strange Case of Dr. Jekyll and Mr. Hyde.txt'
+    }
+];
+
+app.get(`/books`, (req, res) => {
+    res.json(books);
+});
+
+app.listen(port, () => {
+    console.log(`Express-Server lauscht Port ${port}`);
+});
