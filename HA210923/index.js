1260e008eec9a530775fdc232fa644cef74a0ee3
PabloJoelMR <pabloreufer@gmail.com>
2023-09-22 09:27:29 +0200

    index.js final

--- HA210923/index.js
+++ HA210923/index.js
@@ -11,41 +11,50 @@ class book {
         this.id = id,
         this.titel = titel,
         this.author = author,
-        this.source = source,
-        this.filename = filename;
+        this.source = source
     }
 }
 
 
 const library = []
 
-library.push(new book(library.length +1, "Little Woman", "Luisa May Alcot", "./book1.txt", "book1.txt"))
-library.push(new book(library.length +1, "The Complete Works of William Shakespeare","William Shakespeare", "./book2.txt", "book2.txt"))
-library.push(new book(library.length +1, "The Blue Castle", "L. M. Montgomery", "./book3.txt", "book3.txt"))
-library.push(new book(library.length +1, "The Enchanted April", "Elizabeth Von Arnim", "./book4.txt", "book4.txt"));
+library.push(new book(library.length +1, "Little Woman", "Luisa May Alcot", "./book1.txt"))
+library.push(new book(library.length +1, "The Complete Works of William Shakespeare","William Shakespeare", "./book2.txt"))
+library.push(new book(library.length +1, "The Blue Castle", "L. M. Montgomery", "./book3.txt"))
+library.push(new book(library.length +1, "The Enchanted April", "Elizabeth Von Arnim", "./book4.txt"));
 
 
-console.log(library)
-
-let form = '<h1> Meine Bibliothek </h1> <body><ul>'
-    for (let book of library) {
-        form += `<li>${book.titel} by ${book.author} <button id="readButton" type="submit">Lesen</button></li>
-        <br>
-        <script type="text/javascript">document.getElementById("readButton").onclick = function() {
-            location.href = "http://localhost:3000/read/${book.filename}";
-        }
-        </script>`
-    }
-    form += '</ul></body>';
-
+//console.log(library)
 
 app.get("/", (req, res) => {
-    
-    res.send(form)
-})
+    const bookList = library.map((book) =>`
+    <li>
+        <h3>${book.titel}</h3>
+        <p>Author: ${book.author}</p>
+        <a href="/read/${book.source}" target="_blank"><button>Lesen</button></a>
+    </li>
+    `).join('')
 
-app.get(`/read/:${library.book.filename}`, (req, res) => {
-    res.sendFile(library.book.filename, {root: "./"})
+const html = `
+    <html>
+        <head>
+            <title>Meine Bibliothek</title>
+        </head>
+        <body>
+            <h1>Meine Bibliothek</h1>
+            <ul>${bookList}</ul>
+            <br>
+            <a href="/add"><button type="button">Buch hinzufügen</button></a>
+        </body>
+    </html>
+`
+res.send(html);
+
+});
+
+app.get('/read/:file', (req, res) =>{
+    const fileName = req.params.file;
+    res.sendFile(fileName, { root: './'});
 })
 
 
